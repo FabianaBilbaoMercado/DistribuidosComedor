1d368f83de0ea5a1ba2bde2cc5ce46e9998e5fac
Atoncita <141970502+FabianaBilbaoMercado@users.noreply.github.com>
2025-03-16 17:28:49 -0400

    Cambio de interface

--- DistribuidosAlgoritmosAplicacion/DistribuidosAlgoritmos/obj/Debug/net8.0/DistribuidosAlgoritmos.AssemblyInfo.cs
+++ DistribuidosAlgoritmosAplicacion/DistribuidosAlgoritmos/obj/Debug/net8.0/DistribuidosAlgoritmos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DistribuidosAlgoritmos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466506c3451dfde5d7240e0ed5db8823631a87ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("DistribuidosAlgoritmos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DistribuidosAlgoritmos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
